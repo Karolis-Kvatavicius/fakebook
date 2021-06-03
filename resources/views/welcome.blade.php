f1cc77610b8716575340622c1a6e0f653a684a20
Karolis Kvatavicius <karolis.kvatavicius@gmail.com>
2021-06-03 10:17:10 +0300

    fixed .gitignore problem

--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('content')
-    <div class="container-fluid">
-        <div class="row justify-content-center align-items-center">
-            <h1 class="my-5">Welcome To FAKEBOOK</h1>
-        </div>
-    </div>
-@endsection
