0e291a40d3c257df1d4d2dc8d9445ccec761557a
Karolis Kvatavicius <karolis.kvatavicius@gmail.com>
2021-06-08 08:54:32 +0300

    Fixed label for image in create/edit post forms

--- resources/views/create_post.blade.php
+++ resources/views/create_post.blade.php
@@ -32,7 +32,7 @@
 
                             <div class="form-group row">
                                 <label for="'image"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Profile picture') }}</label>
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Post image') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="image" type="file" class="form-control @error('image') is-invalid @enderror"
